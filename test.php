f92fddb67814cde80d6e0e90bc7539ddd015e578
Shumail <shumail.rock@gmail.com>
2018-05-30 15:09:11 +0500

    after reverts test.php

--- test.php
+++ test.php
@@ -1,10 +1,8 @@
 <?php
 $url="http://www.google.com/ig/api?weather=london,england";
-
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-
 $data = curl_exec($ch); 
 print_r($data);
 curl_close($ch); 
