07573f07b4c5c2b0c7d1dc98bf4672ecc57392c0
Shumail <shumail.rock@gmail.com>
2018-05-30 14:56:55 +0500

    removed last comments

--- test.php
+++ test.php
@@ -4,8 +4,6 @@ $url="http://www.google.com/ig/api?weather=london,england";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-
 $data = curl_exec($ch); 
 print_r($data);
-curl_close($ch); 
-////added by shumail rafique
+curl_close($ch);
